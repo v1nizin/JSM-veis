acf7726b8736b529079c302944f7bf23d4e4aadc
v1nizin <viniwick@outlook.com>
2023-08-22 09:25:48 -0300

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<script>
+export default {    name: "Home"    }
+</script>
+
+<template>
+    <div>
+        <h1 class="h1_home">Cadastro de Móveis</h1>
+    </div>
+   
+    <div class="Inputs">
+        <input class="input_movel" type="text" placeholder="Móvel">
+        
+        <input type="text" placeholder="Cor">
+        
+        <input type="text" placeholder="Tamanho">
+        
+        <input type="text" placeholder="Código do Produto">
+
+        <button>Cadastrar</button>
+
+    </div>
+</template>
+
+<style>
+.h1_home {
+    margin-bottom: 80vh;
+    margin-left: 500px;
+    
+}
+.Inputs {
+    margin-top: 30vh;
+    margin-right: 50vh;
+}
+.input_movel {
+    border-radius: 3px;
+    
+}
+</style>
