e054982065e754b115c37d5036fb0c5e41e94fe8
v1nizin <99286223+v1nizin@users.noreply.github.com>
2023-08-28 23:04:13 -0300

    Add files via upload

---
+++ JSM-veis/src/components/Header.vue
@@ -0,0 +1,38 @@
+<script>
+export default {    name: 'Header'  }
+</script>
+
+<template>
+    <div class="Header">
+        <h1 class="JSMoveis">JS Móveis</h1>
+        <h2 class="Pessoal">Pessoal </h2>
+        <button><h3>Perfil</h3></button>
+        <button class="BM"><h3>Móveis</h3></button>
+        <h3 class="Logout">Logout</h3>
+   </div>
+</template>
+
+<style>
+
+    .Header {
+        border-right: 3px solid black;
+        color: black;
+        position: fixed;
+    }
+
+    .JSMoveis {
+        margin-right: 50px;
+    }
+
+    .Pessoal {
+        margin-top: 30px;
+    }
+
+
+
+    .Logout {
+        margin-top: 67.5vh;
+        margin-left: 5vh;
+    }
+
+</style>
---
+++ JSM-veis/src/components/Nav.vue
@@ -0,0 +1,56 @@
+<script>
+export default {    name: 'Nav'    }
+</script>
+
+<template>
+  <nav class="navbar">
+    <ul>
+      <li><router-link to="/">Página Inicial</router-link></li>
+      <li><router-link to="/sobre">Sobre</router-link></li>
+      <li><router-link to="/contato">Contato</router-link></li>
+    </ul>
+    
+    <div class="search-bar">
+      <input class="barradepesquisa" type="text" v-model="searchQuery" placeholder="Pesquisar">
+    </div>
+    <button class="pesquisa" @click="search">Buscar</button>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between; 
+  align-items: center; 
+  padding: 10px 20px;
+  background-color: #3c4daf;
+  color: white;
+  margin-bottom: 900vh;
+  margin-right: 9vh;
+}
+ul {
+  list-style: none;
+  display: flex;
+  gap: 60px; 
+  margin-left: 10px;
+  
+}
+
+ul li a {
+  text-decoration: none;
+  color: white;
+}
+
+.barradepesquisa {
+  margin-top: 9px;
+  margin-left: 100vh;
+}
+
+.pesquisa {
+  margin-left: 14px;
+  height: 50px;
+  
+}
+
+
+</style>
